27ab7ddcd0f637fb8cdf0b887e0d0b29f158dd81
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-15 11:48:43 +0900

    08 MongoDB Cluster Connect

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
 
 const app = express();
+const { MONGO_URI } = config;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB 연결 성공");
+  })
+  .catch((e) => {
+    console.log("MongoDB 연결 실패");
+    console.log(e);
+  });
 
 app.get("/");
 
