293601aea89c34de48290daa9368909f46b4c0f4
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-16 10:41:22 +0900

    10 Router(2)

--- server/app.js
+++ server/app.js
@@ -1,6 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import config from "./config";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+
+// Routes
+import postsRoutes from "./routes/api/post";
 
 const app = express();
 const { MONGO_URI } = config;
@@ -8,13 +15,14 @@ const { MONGO_URI } = config;
 app.use(hpp());
 app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
-app.use(morgan("dev"))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("MongoDB 연결 성공");
@@ -24,6 +32,8 @@ mongoose
     console.log(e);
   });
 
+// Use routes
 app.get("/");
+app.use("/api/post", postsRoutes);
 
 export default app;
--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -1,4 +1,5 @@
 import express from "express";
+import "@babel/polyfill";
 
 // Model
 import Post from "../../models/post";
