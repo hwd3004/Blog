900af6c592295177f03f65fdff0c85778797a8ee
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-16 09:43:05 +0900

    09 Router(1)

--- server/app.js
+++ server/app.js
@@ -5,6 +5,12 @@ import config from "./config";
 const app = express();
 const { MONGO_URI } = config;
 
+app.use(hpp());
+app.use(helmet());
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"))
+app.use(express.json())
+
 mongoose
   .connect(MONGO_URI, {
     useNewUrlParser: true,
