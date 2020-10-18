e71dc455427aa9bf18a3fdc64ec698f99ad6c737
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-18 16:17:46 +0900

    18 Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
 
 const Footer = () => {
+  const thisYear = () => {
+    const year = new Date().getFullYear();
+    return year;
+  };
+
   return (
     <div className="Footer">
-      <div>Footer</div>
+      <Row id="main-footer" className="text-center p-2">
+        <Col>
+          <p>
+            Copyright &copy; <span>{thisYear()}</span>
+          </p>
+        </Col>
+      </Row>
     </div>
   );
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
 
 const Header = () => {
   return (
     <div className="Header">
-      <div>Header</div>
+      <div id="page-header" className="mb-3">
+        <Row>
+          <Col md="6" sm="auto" className="text-center m-auto">
+            <h1>블로그 만들기</h1>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
