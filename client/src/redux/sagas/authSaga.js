cdb30716a6ebfb78671695f74d615bf39a63b22a
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-22 11:42:49 +0900

    27 UserLoading

--- client/src/redux/sagas/authSaga.js
+++ client/src/redux/sagas/authSaga.js
@@ -7,6 +7,9 @@ import {
   LOGOUT_FAILURE,
   LOGOUT_REQUEST,
   LOGOUT_SUCCESS,
+  USER_LOADING_FAILURE,
+  USER_LOADING_REQUEST,
+  USER_LOADING_SUCCESS,
 } from "../type";
 
 // Login
@@ -58,6 +61,43 @@ function* watchlogout() {
   yield takeEvery(LOGOUT_REQUEST, logout);
 }
 
+// User Loading
+const userLoadingAPI = (token) => {
+  console.log(token);
+  
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+  return axios.get("api/auth/user", config);
+};
+
+function* userLoading(loginAction) {
+  try {
+    console.log(loginAction, "userLoading");
+    const result = yield call(userLoadingAPI, loginAction.payload);
+
+    yield put({
+      type: USER_LOADING_SUCCESS,
+      payload: result.data,
+    });
+  } catch (error) {
+    yield put({
+      type: USER_LOADING_FAILURE,
+      payload: error.response,
+    });
+    console.log(error);
+  }
+}
+
+function* watchuserLoading() {
+  yield takeEvery(USER_LOADING_REQUEST, userLoading);
+}
+
 export default function* authSaga() {
-  yield all([fork(watchLoginUser), fork(watchlogout)]);
+  yield all([fork(watchLoginUser), fork(watchlogout), fork(watchuserLoading)]);
 }
