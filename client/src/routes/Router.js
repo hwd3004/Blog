f3635a8889d82f723dc2b60f289f47f4c7a780ff
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-23 13:07:53 +0900

    29 Router

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,13 +2,35 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AppNavbar from "../components/AppNavbar";
+import { Container } from "reactstrap";
+import { Redirect, Route, Switch } from "react-router-dom";
+import PostCardList from "./normalRoute/PostCardList";
+import PostWrite from "./normalRoute/PostWrite";
+import PostDetail from "./normalRoute/PostDetail";
+import CategoryResult from "./normalRoute/CategoryResult";
+import Search from "./normalRoute/Search";
 
 const MyRouter = () => {
   return (
     <>
       <AppNavbar />
       <Header />
-      <h1>Body</h1>
+
+      <Container id="main-body">
+        <Switch>
+          <Route exact path="/" component={PostCardList}></Route>
+          <Route exact path="/post" component={PostWrite}></Route>
+          <Route exact path="/post/:id" component={PostDetail}></Route>
+          <Route
+            exact
+            path="/post/category/:categoryName"
+            component={CategoryResult}
+          ></Route>
+          <Route exact path="/search/:searchTerm" component={Search}></Route>
+          <Redirect from="*" to="/"></Redirect>
+        </Switch>
+      </Container>
+
       <Footer />
     </>
   );
