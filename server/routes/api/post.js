25039244ed9a9f2dce02fc6a35ae66ebc6f8e75f
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-10-27 18:15:04 +0900

    36 Server Image Router

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -7,6 +7,47 @@ import auth from "../../middleware/auth";
 
 const router = express.Router();
 
+import multer from "multer";
+import multerS3 from "multer-s3";
+import path from "path";
+import AWS from "aws-sdk";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_PRIVATE_KEY,
+});
+
+const uploadS3 = multer({
+  storage: multerS3({
+    s3,
+    bucket: "development-bucket-201027/upload",
+    region: "ap-northeast-2",
+    key(req, file, cb) {
+      const ext = path.extname(file.originalName);
+      const basename = path.basename(file.originalName, ext);
+      cb(null, basename + new Date().valueOf() + ext);
+    },
+  }),
+  limits: { fileSize: 100 * 1024 * 1024 },
+});
+
+// @route POST api/post/image
+// @desc Create a Post
+// @access Private
+
+router.post("/image", uploadS3.array("upload", 5), async (req, res, next) => {
+  try {
+    console.log(req.files.map((v) => v.location));
+    res.json({ uploaded: true, url: req.files.map((v) => v.location) });
+  } catch (error) {
+    console.error(error);
+    res.json({ uploaded: false, url: null });
+  }
+});
+
 // api/post
 router.get("/", async (req, res) => {
   const postFindResult = await Post.find();
